0703e55e6cf8053c8787ea1ad440a6d1e8cc17a3
Jeremy Price <github@jerm.org>
2014-03-23 19:02:08 -0400

    making it all work properly

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-sudo easy_install python-setuptools
-sudo pip install ansible
+sudo easy_install pip
+sudo ARCHFLAGS="-Wno-error=unused-command-line-argument-hard-error-in-future" pip install ansible
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew doctor
+open http://getmacapps.com/
 open https://www.hipchat.com/downloads
-open http://www.codeux.com/textual/
 # appstore: mirosoft remote desktop, tweetbot, hashtab
 open http://www.crashplan.com
 open http://ridiculousfish.com/hexfiend/
@@ -11,7 +12,6 @@ open http://ridiculousfish.com/hexfiend/
 open http://tapbots.com/software/tweetbot/mac/
 open http://macromates.com/download
 open http://www.teamviewer.com/en/download/mac.aspx
-open http://www.getcloudapp.com/
 open http://bjango.com/mac/istatmenus/
 open https://www.keepassx.org/
 open https://xquartz.macosforge.org/landing/
@@ -20,7 +20,7 @@ open http://wiki.mikrotik.com/wiki/MikroTik_WinBox_for_Mac_StandAlone
 ## (not needed for winbox anymore) winebottler http://winebottler.kronenberg.org/downloads
 
 # remember http://rogueamoeba.com/  for audio tools
-ansible-playbook -i localhost.inv osx.yml
+ansible-playbook -i localhost.inv osx.yml --ask-sudo-pass
 
 ./osx-hacks.sh
 
